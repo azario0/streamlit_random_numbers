3099dac97e7f29f8c52daef0dd42aef8ca534bc1
Zohir Benmalek <86433428+azario0@users.noreply.github.com>
2022-07-23 16:59:50 +0100

    Update simple_page.py

--- simple_page.py
+++ simple_page.py
@@ -1,21 +1,35 @@
 import streamlit as st
-import numpy as np
 from numpy import random
-st.set_page_config(page_title="simple_cv", layout="wide")
+
+
+
+#parameters of the page
+st.set_page_config(page_title="random number", layout="wide")
 
 st.title('Welcome to my streamlit lab !')
+#this would print a straight line to separate divs
 st.write("---")
 st.header("This is a random number with the function random")
+#generat random number
 x = random.randint(0,5)
+
+
 st.write("This is a random number generated from 0 to 5 : "+str(x))
 st.write("And this is the code used to generate the random number : ")
 code="""
 from numpy import random
 x = random.randint(0,5)"""
 st.code(code,language='python')
+
+
+
 st.write("---")
+
 st.header("This is a 1D array of random numbers between 0 and 4 and it has 50 elements : ")
+#to generate random numbers from 0 to 4 , and it would generate 50 numbers
 x=random.randint(0,5,50)
+
+
 randc1,randc2=st.columns([2,5])
 randc1.subheader("This is the array .")
 randc1.write(x)
@@ -33,6 +47,8 @@ randc1.subheader("This is the array .")
 randc1.write(x)
     """
     st.code(code,language='python')
+
+
 with randc2:
     if st.button('Change the numbers !'):
         x=random.randint(0,5,50)
@@ -51,13 +67,25 @@ if st.button('Change the numbers !'):
 
 
 st.write('---')
+
+
 st.header("And now let's make a 2D array")
-z=random.randint(0,6,size=(5,3))
+
+
+
+
+
 rows = st.slider('Select the number of the rows :', 2, 15, 5)
 columns = st.slider('Select the number of the columns :', 2, 15, 5)
-if st.button('Change the numbers in both rows and colums !'):
-    z=random.randint(0,6,size=(5,3))
+
 z=random.randint(0,6,size=(rows,columns))
+#To change the numbers
+if st.button('Change the numbers in both rows and colums !'):
+    z=random.randint(0,6,size=(rows,columns))
+
+
+
+
 t1,t2=st.columns([5,8])
 t2.subheader("This is the bar chart of the 2D array : ")
 t1.subheader("This is the 2D array of random numbers : ")
